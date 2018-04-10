e971791af741f78e9e3ea20c8dacb1e8183039ac
Unknown <armandpicard71@gmail.com>
2018-04-10 23:00:37 +0200

    ajout couleur de la corne
    c'est trop beau ça change de couleur

--- src/game/entities/Player.py
+++ src/game/entities/Player.py
@@ -11,17 +11,24 @@ class Player(Entity):
         self.level = pygame.Rect(1000, 9170, 4000, 2000)
 
         self.onground = False
+        self.color = (255, 0, 0)
 
+
+        #image pour le rendu
         self.image = {"droite": pygame.image.load('../img/leila.png')}
         self.image["gauche"] = pygame.image.load('../img/leila2.png')
         self.image["droite"] = pygame.transform.scale(self.image["droite"], (100, 150))
         self.image["gauche"] = pygame.transform.scale(self.image["gauche"], (100, 150))
 
+        #rect pour les couleur de la corne
+
+        #rect pour les collisions
         self.hitbox = []
         self.hitbox.append(pygame.Rect(0, 0, 100, 148))
         self.hitbox.append(pygame.Rect(18, 15, 82, 74))
         self.hitbox.append(pygame.Rect(18, 89, 51, 59))
 
+        #rendu pour les hitbox pour les voir
         tete = pygame.Surface((82, 74))
         tete.fill((255, 0, 0, 0.5))
         corp = pygame.Surface((51, 59))
@@ -31,8 +38,10 @@ class Player(Entity):
 
     def render(self):
         if self.vx >= 0:
+            pygame.draw.polygon(self.fenetre, self.color, [[973, 401], [948, 430], [971, 430]])
             self.fenetre.blit(self.image["droite"], (910, 400))
         else:
+            pygame.draw.polygon(self.fenetre, self.color, [[946, 401], [948, 430], [971, 430]])
             self.fenetre.blit(self.image["gauche"], (910, 400))
         ##print("x: ", self.x, "| y: ", self.y)
 
@@ -42,6 +51,7 @@ class Player(Entity):
         lasty = self.y
         lastz = self.z
 
+        #input
         keys = pygame.key.get_pressed()
         if keys[K_a]:
             self.vx = -20
@@ -50,14 +60,54 @@ class Player(Entity):
         if not(keys[K_a] or keys[K_d]):
             self.vx = 0
 
+        if keys[K_w]:
+            self.z += -20
+        if keys[K_s]:
+            self.z += 20
+        if self.z < 0:
+            self.z = 0
+        elif self.z > 1530:
+            self.z = 1530
+        print("z = ", self.z)
+
+
         if keys[K_SPACE]:
             if self.onground:
                 self.vy -= 80
                 self.onground = False
 
+        #calcul de la couleur de la corne
+        r = 0
+        g = 0
+        b = 0
+        if self.z < 255:
+            r = 255
+            g = self.z
+            b = 0
+        elif self.z < 510:
+            r = 510 - self.z
+            g = 255
+            b = 0
+        elif self.z < 765:
+            r = 0
+            g = 255
+            b = self.z - 510
+        elif self.z < 1020:
+            r = 0
+            g = 1020 - self.z
+            b = 255
+        elif self.z < 1275:
+            r = self.z - 1020
+            g = 0
+            b = 255
+        else:
+            r = 255
+            g = 0
+            b = 1530 - self.z
+        self.color = (r, g, b);
+
+        #ajout de la gravité et des vitesses
         self.vy += 9.81
-
-
         self.x += self.vx
         self.y += self.vy
 
