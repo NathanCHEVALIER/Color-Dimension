e721aae410f0e6cb259c5c25754ec86f80068f61
Nathan CHEVALIER <nathanchevalier.lechatelet@gmail.com>
2018-04-30 18:55:09 +0200

    Décors en sprites

--- src/game/map/Map.py
+++ src/game/map/Map.py
@@ -11,7 +11,7 @@ class Map:
         self.level = 0
         self.zone = 0
         self.posZone = 0
-        self.image = {"plateforme": pygame.image.load('../img/plateforme.png'), "piege": pygame.image.load('../img/piege.png'), "colorPlateforme": pygame.image.load('../img/colorPlateforme.png')}
+        self.image = {"sprite": 0, "plateforme": 0, "piege": 0, "colorPlateforme": 0}
         self.rects = {"plateforme": [], "piege": [], "colorPlateforme": []}
         self.enemies = []
         self.enemies.append(Monster(self.fenetre, 3000, 1000, 0))
@@ -37,6 +37,13 @@ class Map:
 
     def loadMap(self, mapId, zoneId):
         content = json.load(open('../data/map.json'))
+        self.image["sprite"] = pygame.image.load('../img/sprite' + mapId + '.png')
+        plateforme = self.image["sprite"].subsurface(300, 300, 100, 50)
+        self.image["plateforme"] = [plateforme, plateforme.get_rect()]
+        piege = self.image["sprite"].subsurface(300, 100, 100, 50)
+        self.image["piege"] = [piege, piege.get_rect()]
+        colorPlateforme = self.image["sprite"].subsurface(300, 200, 100, 50)
+        self.image["colorPlateforme"] = [colorPlateforme, colorPlateforme.get_rect()]
         if zoneId != False:
             return content[mapId][zoneId]
         else:
@@ -54,43 +61,29 @@ class Map:
         self.setPlateforme(data["plateforme"])
         self.setPiege(data["piege"])
         self.setColorPlateforme(data["colorPlateforme"])
-        data = data["plateforme"]
-        plateforme = pygame.transform.scale(self.image["plateforme"], (data["2"][2], data["2"][3]))
-        pos = plateforme.get_rect()
-        pos = pos.move(data["2"][0], data["2"][1])
-        self.zone.blit(plateforme, pos)
 
     def setPlateforme(self, data):
         for c in data:
             for i in range(0, int(data[c][2] / 100)):
-                plateforme = self.image["plateforme"]
-                pos = plateforme.get_rect()
-                pos = pos.move(data[c][0] + (i *100), data[c][1])
+                pos = self.image["plateforme"][1].move(data[c][0] + (i *100), data[c][1])
                 self.rects["plateforme"].append(pos)
-                self.zone.blit(plateforme, pos)
-        print(self.rects)
+                self.zone.blit(self.image["plateforme"][0], pos)
 
     def setPiege(self, data):
         for c in data:
             for i in range(0, int(data[c][2] / 100)):
-                plateforme = self.image["piege"]
-                pos = plateforme.get_rect()
-                pos = pos.move(data[c][0] + (i *100), data[c][1])
+                pos = self.image["piege"][1].move(data[c][0] + (i *100), data[c][1])
                 self.rects["piege"].append(pos)
-                self.zone.blit(plateforme, pos)
-        print(self.rects)
+                self.zone.blit(self.image["piege"][0], pos)
 
     def setColorPlateforme(self, data):
         for c in data:
             for i in range(0, int(data[c][2] / 100)):
-                plateforme = self.image["colorPlateforme"]
-                pos = plateforme.get_rect()
-                pos = pos.move(data[c][0] + (i *100), data[c][1])
+                pos = self.image["colorPlateforme"][1].move(data[c][0] + (i *100), data[c][1])
                 params = [pos, data[c][4]]
                 self.rects["colorPlateforme"].append(params)
                 pygame.draw.rect(self.zone, getColor(data[c][4]), pos)
-                self.zone.blit(plateforme, pos)
-        print(self.rects)
+                self.zone.blit(self.image["colorPlateforme"][0], pos)
 
     def setCamera(self, x, y):
         pos = self.level.get_rect()
